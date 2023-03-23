fe479b88cb2ca49de0fcf11edcb7a7386fa23549
britgand <jonathan.breitgand@gmail.com>
2023-03-23 22:56:39 +0200

    add 8 prompts + hebrew fix

--- logic.js
+++ logic.js
@@ -6,7 +6,7 @@ function func(){
 }
 function changefunc(){
     console.log("hi");
-    const solutions = {"1":"2","3":"4"};
+    const solutions = {"זוויות צמודות משלימות זו את זו ל- ":"180","זוויות קודקודיות":"שוות","במשולש, מול זוויות שוות":"צלעות שוות","במשולש שווה שוקיים, זוויות הבסיס":"שוות","סכום כל שתי צלעות במשולש (?) מהצלע השלישית":"גדול", "במשולש שווה שוקיים , חוצה זווית הראש, התיכון לבסיס והגובה לבסיס":"מתלכדים"};
     let prompt = document.getElementById("textInMid").innerHTML;
     let ans = document.getElementById("textInput").value;
     console.log(ans);
@@ -16,7 +16,7 @@ function changefunc(){
     if (solutions[prompt] == ans){
         var audio = new Audio('ding.mp3');
         audio.play();
-        document.getElementById("textInMid").innerHTML = Object.keys(solutions)[Math.round(Math.random() * 1)]
+        document.getElementById("textInMid").innerHTML = Object.keys(solutions)[Math.round(Math.random() * (Object.keys(solutions).length)-1)]
     }
 
 }
